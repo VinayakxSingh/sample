283c1c0093e6b10913890ddfa4d4044ccd395da2
Vinayak <Vinayaksinghforyou@gmail.com>
2025-08-03 04:00:49 +0530

    scrolling+products-overview changed

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import HomePage from '../pages/Home/homepage';
 import ProductPage from '../pages/products/products-overview';
-import { ServicePage  } from '../pages/Services';
+import { ServicePage  } from '../pages/Services/index';
 import { MethodologyPage as MethodologiesIndex } from '../pages/Methodologies';
 
 const Routing = () => {

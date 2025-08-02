7ba1bac398144d3a70773d9d13410304c40207cf
Vinayak <Vinayaksinghforyou@gmail.com>
2025-08-03 04:22:21 +0530

    SCROLL REMOVED

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,11 @@
 import { Routes } from 'react-router-dom'
 import './App.css';
-import { useEffect } from 'react'
 import Navbar from "./components/headers/Navbar.jsx"
 import Routing from "./routes/routes.jsx"
 import Footer from "./components/Footer/Footer"
-import Lenis from 'lenis'
+
 function App() {
-  useEffect(() => {
-    // Initialize Lenis smooth scrolling
-    const lenis = new Lenis({
-      duration: 1.2, // Smoothness of the scroll (higher value = smoother)
-      easing: (t) => t, // Custom easing function for scrolling
-      smoothWheel: true, // Enable smooth scrolling on wheel events
-    });
 
-    // Create an animation frame loop for smooth scroll updates
-    function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-    requestAnimationFrame(raf);
-
-    return () => {
-      // Cleanup Lenis when the component is unmounted
-      lenis.destroy();
-    };
-  }, []);
 
   return (<>
     <Navbar />
